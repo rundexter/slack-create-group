bb659dc77a0bd8612678484c884a590e6e795f4e
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-26 17:38:43 +0200

    refactore

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var _ = require('lodash');
 var SlackClient = require('slack-api-client');
 
-var authKey = 'slack_api_key';
-
 var pickResult = {
     'ok': 'ok',
     'group.id' : 'group_id',
@@ -18,11 +16,11 @@ module.exports = {
      * @param params
      * @param callback
      */
-    slackMain: function (authOptions, params, callback) {
-        var slack = new SlackClient(authOptions);
+    slackMain: function (params, callback) {
+        var slack = new SlackClient(params.token);
 
         // Follow befriended
-        slack.api.groups.create(params, callback);
+        slack.api.groups.create(_.omit(params, 'token'), callback);
     },
 
     /**
@@ -46,27 +44,6 @@ module.exports = {
     },
 
     /**
-     * Get Auth options from Environment.
-     *
-     * @param dexter
-     * @returns {*}
-     */
-    authOptions: function (dexter) {
-        // slack auth property
-        var authOptions = {};
-
-        if(dexter.environment(authKey)) {
-            // get auth property
-            return dexter.environment(authKey);
-        } else {
-            // catch no-arguments message
-            this.fail('A ' + authKey + ' environment variable is required for this module');
-
-            return false;
-        }
-    },
-
-    /**
      * Allows the authenticating users to follow the user specified in the ID parameter.
      *
      * @param {AppStep} step Accessor for the configuration for the step using this module.  Use step.input('{key}') to retrieve input data.
@@ -74,7 +51,7 @@ module.exports = {
      */
     run: function(step, dexter) {
 
-        this.slackMain(this.authOptions(dexter), step.inputs(), function (error, apiResult) {
+        this.slackMain(step.inputs(), function (error, apiResult) {
             if (error) {
                 // if error - send message
                 this.fail(error);
